3daebbc17655df6dd36a09975fe699af32c22ad7
Egor <e.kupin@nv.dunice.net>
2024-02-09 16:16:00 +0300

    1 lesson

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+let arrayTodo = [];
+
+const addButton = document.querySelector(".add-button");
+const checkbox = document.querySelector('.checkbox'); 
+const inputTodo = document.querySelector('#input');
+const allButton = document.querySelector('.all-button');
+const activeButton = document.querySelector('.active-button');
+const completedButton = document.querySelector('.completed-button');
+
+addButton.addEventListener('click', function () {
+    console.log(inputTodo.value)
+})
+
+
+
+
+
