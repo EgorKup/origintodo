c1c9e15878375f079fb906ea56af292e8be24e5f
Egor <e.kupin@nv.dunice.net>
2024-02-09 17:54:20 +0300

    first project + render

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,36 @@
 let arrayTodo = [];
 
-const addButton = document.querySelector(".add-button");
+
+const buttonAdd = document.querySelector(".add-button");
 const checkbox = document.querySelector('.checkbox'); 
 const inputTodo = document.querySelector('#input');
 const allButton = document.querySelector('.all-button');
 const activeButton = document.querySelector('.active-button');
 const completedButton = document.querySelector('.completed-button');
+const tasksList = document.querySelector('.tasks-list')
 
-addButton.addEventListener('click', function () {
+// --
+buttonAdd.addEventListener('click', function () {
     console.log(inputTodo.value)
-})
+    inputTodo.value = '';
+    render();
+});
+
+
+
+function render () {
+    arrayTodo;
+    tasksList.innerHTML +=
+
+    `<div>
+        <input type="checkbox" id ${1}>
+        <input type="text">
+        <button class='checked'>fsdfsdffs</button>
+    </div>`
+
+}
+
+
 
 
 
